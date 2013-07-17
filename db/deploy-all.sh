05829a96c6c717d3374e55631e2d070cc761d23d
saxonmatt <saxonmatt@gmail.com>
2013-07-18 00:13:09 +0100

    Line ending fun.

--- db/deploy-all.sh
+++ db/deploy-all.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # 1. List of .txt files containing sql to execute on the servicestackdb.
 mysql --user=servicestack --password=servicestackpw servicestackdb < /vagrant/db/0001-0.1.0/0001-create-table-helloworld.txt
-
